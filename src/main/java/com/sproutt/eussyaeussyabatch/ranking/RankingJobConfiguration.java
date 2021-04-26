158b38620da5ce60e25b244f10a82e3645412a27
Ilisha29 <94konzern@gmail.com>
2021-04-27 04:37:16 +0900

    feat : JdbcCursorItemReader && add : example data

--- src/main/java/com/sproutt/eussyaeussyabatch/ranking/RankingJobConfiguration.java
+++ src/main/java/com/sproutt/eussyaeussyabatch/ranking/RankingJobConfiguration.java
@@ -1,5 +1,6 @@
 package com.sproutt.eussyaeussyabatch.ranking;
 
+import com.sproutt.eussyaeussyabatch.entity.Grass;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -7,9 +8,14 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,9 +23,10 @@ import org.springframework.context.annotation.Configuration;
 public class RankingJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     @Bean
-    public Job saveRankingJob() {
+    public Job saveRankingJob() throws Exception {
         return this.jobBuilderFactory.get("saveRankingJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.saveRankingStep())
@@ -27,11 +34,29 @@ public class RankingJobConfiguration {
     }
 
     @Bean
-    public Step saveRankingStep() {
+    public Step saveRankingStep() throws Exception {
         return this.stepBuilderFactory.get("saveRangkinStep")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>>Step");
-                    return RepeatStatus.FINISHED;
-                }).build();
+                .<Grass, Grass>chunk(10)
+                .reader(jdbcCursorItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private JdbcCursorItemReader<Grass> jdbcCursorItemReader() throws Exception {
+        JdbcCursorItemReader<Grass> itemReader = new JdbcCursorItemReaderBuilder<Grass>()
+                .name("jdbcCursorItemReader")
+                .dataSource(dataSource)
+                .sql("SELECT id, member_id, date, complete_count from grass")
+                .rowMapper((rs, rowNum) -> new Grass(
+                        rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getInt(4)))
+                .build();
+        itemReader.afterPropertiesSet();
+        return itemReader;
+    }
+
+    private ItemWriter<Grass> itemWriter() {
+        return items -> log.info(items.stream()
+                .map(Grass::toString)
+                .collect(Collectors.joining("\n")));
     }
 }
